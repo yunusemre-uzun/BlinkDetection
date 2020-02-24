8c7d4a5d886b0f23da1dd9b9a47a22b270fc62c8
Yunus Emre Uzun <yunus.uzun@metu.edu.tr>
2020-02-24 16:46:17 +0300

    Multiprocess implementation to test

--- blink_detection_dlib_multprocess.py
+++ blink_detection_dlib_multprocess.py
@@ -8,6 +8,9 @@ import time
 import dlib
 import imutils
 
+FACE_BOX = None
+IS_REAL = False
+
 def getVideoStream(args):
     if args["video"]=="":
         # Choose the camera as default
@@ -61,52 +64,31 @@ def startVideoStream(vs, detector):
         success, frame = vs.read()
 
     cv2.destroyAllWindows()
-    sum = 0
-    for runtime in runtime_array:
-        sum += runtime
-    avg = sum / len(runtime_array)
-    print ("Avg blink detection time:" , avg)
-    sum = 0
-    for runtime in face_detection_runtime_array:
-        sum += runtime
-    avg = sum / len(face_detection_runtime_array)
-    print ("Avg face detection time:" , avg)
+    avgCalculations(runtime_array, face_detection_runtime_array)
 
-def startCameraSteam(vs, detector, gap):
-    face_box = None
+
+def startCameraSteam(vs, detector):
     i = 0
     runtime_array = []
     face_detection_runtime_array = []
     is_real = False
-    print(vs)
-    print("Sleeping for ", 2.0+gap)
-    time.sleep(2.0+gap)
+    time.sleep(2.0)
     while True:
-        frame = vs.read()
-        print(frame)
-        frame = imutils.resize(frame, width=250)
-        print("Frame: ", i)
-        i+=1
-        #frame = imutils.resize(frame, width=450)
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # detect faces in the rgb frame
         start = time.time()
-        rects = detector(frame_gray, 0)
-        stop = time.time()
-        face_detection_runtime_array.append(stop-start)
-        for rect in rects:
-            if face_box is None:
-                face_box = FaceBox(None, frame_gray, args["shape_predictor"], rect)
-            else:
-                face_box.updateFrame(frame_gray)
-                face_box.updateRect(None, rect)
-            start = time.time()
-            check_liveness = face_box.checkFrame() 
-            stop = time.time()
-            runtime_array.append(stop-start)
-            if check_liveness :
-                print("Real")
-                is_real = True
+        frame = getFrame(vs)
+        p1 = Process(target=processFrame, args=(frame_gray))
+        frame = getFrame(vs)
+        p2 = Process(target=processFrame, args=(frame_gray))
+        frame = getFrame(vs)
+        p3 = Process(target=processFrame, args=(frame_gray))
+        p1.start()
+        p2.start()
+        p3.start()
+        p1.join()
+        p2.join()
+        p3.join()
+        end = time.time()
+        print("Time elapsed: ", stop-start)
         cv2.imshow("Frame", frame_gray)
         key = cv2.waitKey(1) & 0xFF
         # if the `q` key was pressed, break from the loop
@@ -115,6 +97,29 @@ def startCameraSteam(vs, detector, gap):
 
     cv2.destroyAllWindows()
     vs.stop()
+    avgCalculations(runtime_array, face_detection_runtime_array)
+
+def getFrame(vs):
+    frame = vs.read()
+    frame = imutils.resize(frame, width=250)
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    return frame_gray
+
+def processFrame(frame):
+    rects = detector(frame, 0)
+    for rect in rects:
+        if FACE_BOX is None:
+            FACE_BOX = FaceBox(None, frame_gray, args["shape_predictor"], rect)
+        else:
+            FACE_BOX.updateFrame(frame_gray)
+            FACE_BOX.updateRect(None, rect)
+        check_liveness = FACE_BOX.checkFrame() 
+        if check_liveness :
+            print("Real")
+            IS_REAL = True
+    return None
+
+def avgCalculations(runtime_array, face_detection_runtime_array):
     sum = 0
     for runtime in runtime_array:
         sum += runtime
@@ -125,6 +130,7 @@ def startCameraSteam(vs, detector, gap):
         sum += runtime
     avg = sum / len(face_detection_runtime_array)
     print ("Avg face detection time:" , avg)
+
 
 def main(args):
     print("[INFO] loading facial landmark predictor...")
@@ -135,15 +141,7 @@ def main(args):
     if(file_stream):
         startVideoStream(vs, detector)
     else:
-        p1 = Process(target=startCameraSteam, args=(vs, detector, 0))
-        p2 = Process(target=startCameraSteam, args=(vs, detector, 0.1))
-        p3 = Process(target=startCameraSteam, args=(vs, detector, 0.2))
-        p1.start()
-        p2.start()
-        p3.start()
-        p1.join()
-        p2.join()
-        p3.join()
+        startCameraSteam(vs, detector)
     
 
 if __name__ == "__main__":
