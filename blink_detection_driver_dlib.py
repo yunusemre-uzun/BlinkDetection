deb6729cd6a7c9e46ee7abbf32206cc8e1d8b11d
Yunus Emre Uzun <yunus.uzun@metu.edu.tr>
2020-02-24 17:07:17 +0300

    Paramater tuning

--- blink_detection_driver_dlib.py
+++ blink_detection_driver_dlib.py
@@ -80,7 +80,7 @@ def startCameraSteam(vs, detector):
     time.sleep(2.0)
     while True:
         frame = vs.read()
-        frame = imutils.resize(frame, width=300)
+        frame = imutils.resize(frame, width=200)
         print("Frame: ", i)
         i+=1
         #frame = imutils.resize(frame, width=450)
